045ab1b5f28581367b07fca49d854079f7d53adb
Luiz <37978726+luizmariz@users.noreply.github.com>
2019-05-20 16:18:29 -0300

    change: PureComponent on Quiz

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,9 +1,9 @@
 const logger = store => next => action => {
-  console.group(action.type);
-    console.log('Action: ', action);
-    const returnValue = next(action);
-    console.log('New State: ', store.getState());
-  console.groupEnd();
+  // console.group(action.type);
+  //   console.log('Action: ', action);
+  const returnValue = next(action);
+  //   console.log('New State: ', store.getState());
+  // console.groupEnd();
 
   return returnValue;
 };
